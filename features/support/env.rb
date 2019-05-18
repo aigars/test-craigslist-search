342243c3472c57ac61e83ad8307df470fd4d3b65
Aigars <aigars.susters@veset.tv>
2019-05-18 12:16:33 +0300

    Add global hook for web browser; Kill cucumber if test fails

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,16 @@
 require 'selenium-webdriver'
 require 'rspec'
 
+driver = Selenium::WebDriver.for :chrome
+
 Before do
-  @driver = Selenium::WebDriver.for :chrome
+  @driver = driver
 end
 
-After do
-  @driver.quit
+After do |scenario|
+  Cucumber.wants_to_quit = true if scenario.failed? #exit cucumber i one of tests failed
+end
+
+at_exit do
+  driver.quit
 end
